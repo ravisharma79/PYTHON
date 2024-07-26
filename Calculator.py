ba4c6c0812626193c878165ef71360a70f081981
RAVI SHARMA <ravisharma50080@gmail.com>
2024-07-26 11:30:08 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,18 @@
+a=int (input("ENTER YOUR NUMBER: "))
+b=int (input("ENTER YOUR SECOND NUMBER: "))
+
+
+ans1=(a+b)
+print( "addition of" ,a, "and" ,b, "is", ans1 )
+
+ans2=(a-b)
+print( "minus of" ,a, "and" ,b, "is", ans2 )
+
+ans3=a*b
+print( "subtraction of" ,a, "and" ,b, "is", ans3 )
+
+ans4=a/b
+print( "division of" ,a, "and" ,b, "is", ans4 )
+
+ans5=a%b
+print( "modules of" ,a, "and" ,b, "is", ans5 )
